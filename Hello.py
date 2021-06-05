e1fcfa819ba9876f803b5d1e0d37b7249da26290
MalithaDilshan <malithakumarasiri93@gmail.com>
2021-06-05 23:35:44 +0530

    Initial commit for the AlgoExpert

---
+++ Hello.py
@@ -0,0 +1,17 @@
+"""
+Initial commit for the AlgoExpert - This is a self study project using advanced Python programming
+This will include,
+1. Data structures and algorithms
+2. UI programmings
+3. Web programming
+4. Threads and parallel programming techniques
+5. Full stack development related studies
+6. Data bases
+7. Data mining techniques
+6. Machine learning
+7. AI and ANN
+8. Other studies
+"""
+
+a = 'Hello SpentX AlgoExpert..\nHow are you?'
+print(a)
